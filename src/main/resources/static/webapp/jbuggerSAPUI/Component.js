48f230f6a32f7d507d835da1b596c188bef759f9
Adam <asd@gmail.com>
2018-10-04 22:39:15 +0300

    Add working second column of FlexibleColumnLayout in the View Bugs section

--- src/main/resources/static/webapp/jbuggerSAPUI/Component.js
+++ src/main/resources/static/webapp/jbuggerSAPUI/Component.js
@@ -1,7 +1,8 @@
 sap.ui.define([
 	'sap/ui/core/UIComponent',
-	'sap/f/FlexibleColumnLayoutSemanticHelper'],
-function(UIComponent,FlexibleColumnLayoutSemanticHelper) {
+	'sap/f/FlexibleColumnLayoutSemanticHelper',
+	'sap/ui/model/json/JSONModel'],
+function(UIComponent,FlexibleColumnLayoutSemanticHelper,JSONModel) {
 	"use strict";
 	
 	var Component = UIComponent.extend("jbuggerSAPUI.Component", {
@@ -11,9 +12,16 @@ function(UIComponent,FlexibleColumnLayoutSemanticHelper) {
 		
 		init: function () {
 			UIComponent.prototype.init.apply(this, arguments);
+			var oModel = new JSONModel();
+			this.setModel(oModel);
 			this.getRouter().initialize();
 		},
-		
+		createContent: function () {
+			return sap.ui.view({
+				viewName: "jbuggerSAPUI.view.ManageBugs",
+				type: "XML"
+			});
+		},
 		/**
 		* Returns an instance of the semantic helper
 		* @returns {sap.f.FlexibleColumnLayoutSemanticHelper} An instance of the semantic helper
