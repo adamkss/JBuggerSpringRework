09bc698115240567e13af6bbcff51e1c71d23f0f
Kiss ADAM <adam.kiss@msg.group>
2019-02-20 12:47:28 +0200

    Modify selected and inactive labels styles

--- src/main/resources/static/webapp/jbugger/src/LabelShort.js
+++ src/main/resources/static/webapp/jbugger/src/LabelShort.js
@@ -4,16 +4,30 @@ import styled from 'styled-components';
 const ParentDiv = styled.div`
     height: 20px;
     border-radius: 7px;
-    background-color: ${props => props.backgroundColor || "orange"};
+    background-color: ${props => {
+        if (props.selectable && !props.selected) {
+            return "grey";
+        }
+        return props.backgroundColor || "orange";
+    }};
     color: ${props => {
+        if (props.selectable && !props.selected)
+            return "#FFFFFF";
+
         if (props.backgroundColor) {
             const red = parseInt(props.backgroundColor.substring(1, 2), 16);
             const green = parseInt(props.backgroundColor.substring(3, 2), 16);
             const blue = parseInt(props.backgroundColor.substring(5, 2), 16);
-            if ((red * 0.299 + green * 0.587 + blue * 0.114) < 186)
-                return "#ffffff";
+            if ((red * 0.299 + green * 0.587 + blue * 0.114) >= 186)
+                return "#000000";
         }
-        return "#000000";
+
+        return "#FFFFFF";
+    }}
+    opacity: ${props => {
+        if (props.selectable && !props.selected)
+            return "0.5";
+        return "1";
     }}
     font-size: 0.78rem;
     font-family: "Roboto", "Helvetica", "Arial", sans-serif;
@@ -26,8 +40,6 @@ const ParentDiv = styled.div`
     padding-bottom: 1px;
     margin-left: 3px;
     cursor: ${props => props.selectable ? "pointer" : "initial"};
-    ${props => props.selected && "border: solid 2px #4a148c;"}
-    ${props => props.selected && "box-sizing: content-box;"}
 `;
 
 
