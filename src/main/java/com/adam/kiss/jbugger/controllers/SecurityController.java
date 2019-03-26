b2fac1ccb47c57129035fcf6df65ac037c9f17fc
Adam KISS <adam.kiss@msg.group>
2019-03-26 23:41:19 +0200

    Add cross origin

--- src/main/java/com/adam/kiss/jbugger/controllers/SecurityController.java
+++ src/main/java/com/adam/kiss/jbugger/controllers/SecurityController.java
@@ -9,15 +9,13 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/auth")
+@CrossOrigin
 public class SecurityController {
     @Autowired
     private AuthenticationManager authenticationManager;
